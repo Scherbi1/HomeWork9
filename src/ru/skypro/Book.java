4bc4d889cf623e01a30962163144c4ed0ca73448
Scherbi <shherbakov99ilya@mail.ru>
2022-07-03 14:12:42 +0300

    Выполнил ДЗ

--- src/ru/skypro/Book.java
+++ src/ru/skypro/Book.java
@@ -1,4 +1,28 @@
 package ru.skypro;
 
 public class Book {
+    private String title;
+    private Author Author;
+    private int year;
+
+    public Book(String title, Author Author, int year) {
+        this.title =title;
+        this.year=year;
+    }
+     public String getTitle(){
+        return title;
+     }
+
+
+    /*public Author getAuthor() {
+        return Author;
+    }*/
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
 }
--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -4,5 +4,20 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Author fred = new Author("Fred" , "Vargas");
+    Book girl = new Book("Человек наизнанку", fred , 1999);
+        System.out.println("Назвиние книги - " + girl.getTitle());
+        System.out.println("Имя автора - " + fred.getName());
+        System.out.println("Фамилия автора - " + fred.getLastName());
+        System.out.println("год выпуска - " + girl.getYear());
+        girl.setYear(2000);
+        System.out.println("год выпуска - " + girl.getYear());
+        Author alex = new Author("Александр" , "Пушкин");
+        Book man = new Book("Дубровсикй", alex , 1833);
+        System.out.println("Назвиние книги - " + man.getTitle());
+        System.out.println("Имя автора - " + alex.getName());
+        System.out.println("Фамилия автора - " + alex.getLastName());
+        System.out.println("год выпуска - " + man.getYear());
+
     }
 }
