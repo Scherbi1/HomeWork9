4365d2a47d53738704c8e7bd8a5b799f7863996b
Scherbi <shherbakov99ilya@mail.ru>
2022-07-04 19:58:07 +0300

    Выполнил ДЗ 10

--- src/ru/skypro/Book.java
+++ src/ru/skypro/Book.java
@@ -1,5 +1,7 @@
 package ru.skypro;
 
+import java.util.Objects;
+
 public class Book {
     private String title;
     private Author Author;
@@ -8,15 +10,16 @@ public class Book {
     public Book(String title, Author Author, int year) {
         this.title =title;
         this.year=year;
+        this.Author=Author;
     }
      public String getTitle(){
         return title;
      }
 
 
-    /*public Author getAuthor() {
+    public Author getAuthor() {
         return Author;
-    }*/
+    }
 
     public int getYear() {
         return year;
@@ -24,5 +27,25 @@ public class Book {
 
     public void setYear(int year) {
         this.year = year;
+    }
+
+
+
+    public String toString() {
+
+        return "Назвиние книги - " + title + " " +  Author + ". год выпуска - " + year;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return year == book.year && title.equals(book.title) && Author.equals(book.Author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, Author, year);
     }
 }
--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -5,7 +5,8 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         Author fred = new Author("Fred" , "Vargas");
-    Book girl = new Book("Человек наизнанку", fred , 1999);
+        Book girl = new Book("Человек наизнанку", fred, 1999);
+        Book girl2 = new Book("Человек наизнанку", fred , 1999);
         System.out.println("Назвиние книги - " + girl.getTitle());
         System.out.println("Имя автора - " + fred.getName());
         System.out.println("Фамилия автора - " + fred.getLastName());
@@ -18,6 +19,9 @@ public class Main {
         System.out.println("Имя автора - " + alex.getName());
         System.out.println("Фамилия автора - " + alex.getLastName());
         System.out.println("год выпуска - " + man.getYear());
-
+        System.out.println(girl);
+        System.out.println(man);
+        System.out.println(girl.hashCode() == girl2.hashCode());
+        System.out.println(girl.equals(girl2));
     }
 }
